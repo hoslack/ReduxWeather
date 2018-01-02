f99d979e83cb024409fb6ad7536e6b9176b89284
hoslack <hoslackochieng@gmail.com>
2018-01-02 23:11:05 +0200

    Display graphical data

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMaps from '../components/google_map';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
 		const temps = cityData.list.map(weather => weather.main.temp);
-		console.log(temps);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const { lon, lat } = cityData.city.coord;
+
 		return (
 			<tr key={name}>
-				<td>{name}</td>
 				<td>
-					<Chart data={temps} color="orange" />
+					<GoogleMaps lon={lon} lat={lat} />
+				</td>
+				<td>
+					<Chart data={temps} color="orange" units="K" />
+				</td>
+				<td>
+					<Chart data={pressures} color="green" units="hPa" />
+				</td>
+				<td>
+					<Chart data={humidities} color="black" units="%" />
 				</td>
 			</tr>
 		);
